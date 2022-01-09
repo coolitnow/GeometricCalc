99a0a50196d8c7039dcc78f1f00fbc3aa25bc8e5
Caitlin Bender <85584674+coolitnow@users.noreply.github.com>
2022-01-09 12:37:37 -0500

    Add Circle class

---
+++ GeoCalcLibrary/Circle.cs
@@ -0,0 +1,15 @@
+namespace GeoCalcLibrary
+{
+    public class Circle
+    {
+        public double Radius { get; set;}
+
+        public double Diameter { get; set;}
+
+        public double CalcRadius(double Diameter)
+        {
+            double radius = Diameter/2;
+            return Radius = radius;
+        }
+    }
+}

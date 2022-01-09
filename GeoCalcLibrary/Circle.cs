352aec94251de709d0324c339cf0a53e50093550
Caitlin Bender <85584674+coolitnow@users.noreply.github.com>
2022-01-09 14:56:04 -0500

    Refactor Circle Area choice to a method and place that method in the Circle claass.

--- GeoCalcLibrary/Circle.cs
+++ GeoCalcLibrary/Circle.cs
@@ -5,7 +5,7 @@ namespace GeoCalcLibrary
     public class Circle
     {
         public double Radius { get; set;}
-        
+
 
         public double Area(double radius)
         {
@@ -13,6 +13,29 @@ namespace GeoCalcLibrary
             return Math.Round(area, 2);
         }
 
+        public void CircleArea()
+        {
+            double cirArea = 0;
+            bool canConvert;
+             //do while loop to ensure a double as an answer
+            do
+            {
+                Console.Write("Please enter the radius of the circle: ");
+                string ans = Console.ReadLine();
+                //check if answer is a double
+                canConvert = double.TryParse(ans, out double rad);
+                if (canConvert != true)
+                {
+                    Console.WriteLine("Sorry, try again. This time type a number like 8 or 5.2. The calculation works with any measurement (feet, meters, etc), so don't worry about that!");
+                }
+                else if (canConvert == true)
+                {
+                    Radius = Convert.ToDouble(ans);
+                    cirArea = Area(Radius);
+                }
+            } while (canConvert != true);
 
+            Console.WriteLine($"The area of your circle is {cirArea} (your measurement of choice) squared.");
+        }
     }
 }
--- GeoCalcUI/Program.cs
+++ GeoCalcUI/Program.cs
@@ -8,14 +8,6 @@ namespace GeoCalcUI
     {
         static void Main(string[] args)
         {
-            // Square sqr = new Square
-            // {
-            //     Length = 12,
-            //     Width = 4
-            // };
-            
-            // Console.WriteLine($"{sqr.Length} is the Length and {sqr.Width} is the width");
-
             Console.WriteLine("Welcome to the Geometry Calculator! \nCan I calculate every geometric problem??");
             Thread.Sleep(1000);
             Console.WriteLine("...");
@@ -28,17 +20,17 @@ namespace GeoCalcUI
             Console.WriteLine("Let's get started");
             Thread.Sleep(1000);
 
+            //a bool to keep the while loop open, and also as a tool to close it
             bool calculate = true;
             
-
             while (calculate == true)
             {
-                 Console.WriteLine("What would you like to calculate?\na) Area of a Circle\nd) Quit");
+                 Console.WriteLine("What would you like to calculate?\na) Area of a Circle\nb) Area of a Rectangle\nd) Quit");
                 string shape2Calc = Console.ReadLine().ToLower();
 
-                while (shape2Calc != "a" && shape2Calc != "d")
+                while (shape2Calc != "a" && shape2Calc != "b" && shape2Calc != "d")
                 {
-                    Console.WriteLine("I'm sorry, please choose the corresponding letter with your desired geometric calculation:\na) Find the area of a Circle\nd) Quit");
+                    Console.WriteLine("I'm sorry, please choose the corresponding letter with your desired geometric calculation:\na) Find the area of a Circle\nb) Area of a Rectangle\nd) Quit");
                     shape2Calc = Console.ReadLine().ToLower();
                 }
                 
@@ -49,27 +41,8 @@ namespace GeoCalcUI
                     //add ability to calculate by radius or diameter
                     //insantiate new circle
                     Circle crcl = new Circle();
-                    //have to set the Diameter here in order to change it in the Do loop and keep info after loop
-                    crcl.Radius = 0;
                     
-                    //do while loop to ensure a double as an answer
-                    do
-                    {
-                        Console.Write("Please enter the radius of the circle: ");
-                        string ans = Console.ReadLine();
-                        //check if answer is a double
-                        canConvert = double.TryParse(ans, out double radius);
-                        if (canConvert != true)
-                        {
-                            Console.WriteLine("Sorry, try again. This time type a number like 8 or 5.2. The calculation works with any measurement (feet, meters, etc), so don't worry about that!");
-                        }
-                        else if (canConvert == true)
-                        {
-                            crcl.Radius = Convert.ToDouble(ans);
-                        }
-                    } while (canConvert != true);
-
-                    Console.WriteLine($"The area of your circle is {crcl.Area(crcl.Radius)} (your measurement of choice) squared.");
+                    crcl.CircleArea();
                     
                 }
                 else if (shape2Calc == "d")
